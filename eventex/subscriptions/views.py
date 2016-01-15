6be86e0ada5dab27597e4d86281583ba0224432e
thiago oliveira <oliveiravicente.net@gmail.com>
2016-01-15 18:50:28 -0200

    fixed indentity views.py

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -14,16 +14,17 @@ def subscribe(request):
     else:
         return new(request)
 
+
 def create(request):
     form = SubscriptionForm(request.POST)
 
     if not form.is_valid():
-       return render(request, 'subscriptions/subscription_form.html',
-                  {'form': form})
+        return render(request, 'subscriptions/subscription_form.html',
+                      {'form': form})
 
     _send_mail('Confirmação de Inscrição!',
-                settings.DEFAULT_FROM_MAIL,
-                form.cleaned_data['email'], 'subscriptions/subscription_message.txt',
+               settings.DEFAULT_FROM_MAIL,
+               form.cleaned_data['email'], 'subscriptions/subscription_message.txt',
                form.cleaned_data)
 
     messages.success(request, 'Inscrição Realizada com sucesso!')
@@ -34,7 +35,8 @@ def new(request):
     return render(request, 'subscriptions/subscription_form.html',
                   {'form': SubscriptionForm()})
 
-def _send_mail(subject, from_ , to, tamplate_name, context):
+
+def _send_mail(subject, from_, to, tamplate_name, context):
     body = render_to_string(tamplate_name, context)
-    mail.send_mail(subject, body, from_, ['contato@eventex.com', from_ ])
+    mail.send_mail(subject, body, from_, ['contato@eventex.com', to])
 
