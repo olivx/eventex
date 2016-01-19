02eeaab9a457f4a0988fe3b3a69ae1c620948b8e
thiago oliveira <oliveiravicente.net@gmail.com>
2016-01-19 01:57:44 -0200

    search_fieds em SubscriptionsModelAdmin

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -5,7 +5,7 @@ from eventex.subscriptions.models import Subscrition
 
 class SubscriptionsModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'cpf', 'phone', 'created_at', 'subscribe_today')
-    search_fields = ('nome', 'email', 'cpf', 'phone', 'created_at')
+    search_fields = ('name', 'email', 'cpf', 'phone', 'created_at')
     ordering = ('-created_at',)
     date_hierarchy = 'created_at'
     list_filter = ('created_at',)
