dc12037aa5a26e3f5488653e20923e93f74e966e
thiago oliveira <oliveiravicente.net@gmail.com>
2016-02-09 22:34:15 -0200

    refatora with modelForm

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Subscription
 from django.core.exceptions import ValidationError
 
 
@@ -9,7 +10,7 @@ def validate_cpf(value):
         raise ValidationError('CPF deve conter 11 numeros.', 'length')
 
 
-class SubscriptionForm(forms.Form):
+class SubscriptionFormOld(forms.Form):
     name = forms.CharField(label="Nome")
     cpf = forms.CharField(label="CPF", validators=[validate_cpf])
     email = forms.EmailField(label="Email", required=False)
@@ -27,3 +28,22 @@ class SubscriptionForm(forms.Form):
             raise ValidationError('E-mail ou Telefone deve ser informado.')
 
         return self.cleaned_data
+
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscription
+        fields = ('name', 'cpf', 'email', 'phone')
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        words = [w.capitalize() for w in name.split()]
+        return ' '.join(words)
+
+    def clean(self):
+        if not self.cleaned_data.get('email') and \
+            not self.cleaned_data.get('phone'):
+            raise ValidationError('E-mail ou Telefone deve ser informado.')
+
+        return self.cleaned_data
+
+
